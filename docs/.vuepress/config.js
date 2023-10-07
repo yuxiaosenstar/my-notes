6da7649386c8a2345c4b6f3acc05eeb8efcaf7d9
yuchangchang <1346693197@qq.com>
2023-10-08 00:19:57 +0800

    feat:init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,6 +3,7 @@ module.exports = {
   description: '博客 分享 读后感 成长规划',
   dest: './dist',
   base: '/my-notes/',
+  theme: 'reco',
   themeConfig: {
     sidebar: ['/dom/'],
     // 假定 GitHub。也可以是一个完整的 GitLab 网址
