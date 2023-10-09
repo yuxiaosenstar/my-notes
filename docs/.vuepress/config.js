631254142e276ef589fe226a1525f4b41da4f245
yuchangchang <yuchang251314@163.com>
2023-10-09 11:08:29 +0800

    feat:init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,9 +1,33 @@
+const moment = require('moment')
+
 module.exports = {
   title: '前端工程师成长路线',
   description: '博客 分享 读后感 成长规划',
   dest: './dist',
   base: '/my-notes/',
   theme: 'reco',
+  head: [
+    [
+      'meta',
+      {
+        name: 'description',
+        itemprop: 'description',
+        content: '前端工程师成长路线',
+      },
+    ],
+    ['meta', { itemprop: 'name', content: '前端工程师成长路线' }],
+  ],
+  plugins: [
+    [
+      '@vuepress/last-updated',
+      {
+        transformer: (timestamp, lang) => {
+          moment.locale(lang)
+          return moment(timestamp).format('YYYY-MM-DD HH:mm:SS')
+        },
+      },
+    ],
+  ],
   themeConfig: {
     sidebar: [
       '/css/',
@@ -18,6 +42,7 @@ module.exports = {
       '/vue/',
       '/capture/',
     ],
+    lastUpdated: '上次更新时间',
     // 假定 GitHub。也可以是一个完整的 GitLab 网址
     repo: 'https://github.com/yuxiaosenstar/my-notes',
   },
